a8deadefb3eba7c3acce2565c32d1c83c7ef99e5
anas benlemlih <anas.benlemlih@gmail.com>
2025-04-08 14:44:29 +0200

    first commit

---
+++ src/main/java/com/art/gestionart/GestionartApplication.java
@@ -0,0 +1,13 @@
+package com.art.gestionart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/art/gestionart/GestionartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.art.gestionart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
